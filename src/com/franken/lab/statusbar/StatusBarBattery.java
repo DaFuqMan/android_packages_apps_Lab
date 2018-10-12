9887072e1591da23a76c8032fb2eac4fb66a157d
nexx <nexx@users.noreply.github.com>
2018-10-11 22:03:29 -0400

    BatteryMeterDrawable: add hidden option (2/2)

--- src/com/franken/lab/statusbar/StatusBarBattery.java
+++ src/com/franken/lab/statusbar/StatusBarBattery.java
@@ -35,6 +35,7 @@ public class StatusBarBattery extends SettingsPreferenceFragment implements
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
     private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 2;
+    private static final int STATUS_BAR_BATTERY_STYLE_HIDDEN = 3;
 
     private ListPreference mStatusBarBatteryShowPercent;
     private ListPreference mStatusBarBattery;
@@ -93,8 +94,9 @@ public class StatusBarBattery extends SettingsPreferenceFragment implements
         return false;
     }
 
-    private void enableStatusBarBatteryDependents(int batteryIconStyle) {
-        if (batteryIconStyle == STATUS_BAR_BATTERY_STYLE_TEXT) {
+     private void enableStatusBarBatteryDependents(int batteryIconStyle) {
+        if (batteryIconStyle == STATUS_BAR_BATTERY_STYLE_TEXT
+                || batteryIconStyle == STATUS_BAR_BATTERY_STYLE_HIDDEN) {
             mStatusBarBatteryShowPercent.setEnabled(false);
         } else {
             mStatusBarBatteryShowPercent.setEnabled(true);
