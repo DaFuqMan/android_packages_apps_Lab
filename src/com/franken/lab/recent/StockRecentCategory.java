757bb0dccaa68699706bbfbbdf0df6ccdd490546
Dabug123 <clarkbrunswick55@gmail.com>
2018-10-23 22:56:27 -0400

    remove fab as it doesnt even go with p style
    hopefully moving forward we get a more p style method

--- src/com/franken/lab/recent/StockRecentCategory.java
+++ src/com/franken/lab/recent/StockRecentCategory.java
@@ -16,21 +16,15 @@
 
 package com.franken.lab.recent;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.ContentResolver;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.os.SystemProperties;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
-import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
-import java.util.Arrays;
-import java.util.HashSet;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
@@ -38,10 +32,6 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class StockRecentCategory extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
-
-    private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
-    private ListPreference mRecentsClearAllLocation;
-    private SwitchPreference mRecentsClearAll;
 
     @Override
     public int getMetricsCategory() {
@@ -53,17 +43,6 @@ public class StockRecentCategory extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.stock_recent);
-
-        ContentResolver resolver = getActivity().getContentResolver();
-
-        // clear all recents
-        mRecentsClearAllLocation = (ListPreference) findPreference(RECENTS_CLEAR_ALL_LOCATION);
-        int location = Settings.System.getIntForUser(resolver,
-                Settings.System.RECENTS_CLEAR_ALL_LOCATION, 3, UserHandle.USER_CURRENT);
-        mRecentsClearAllLocation.setValue(String.valueOf(location));
-        mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
-        mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
-
     }
 
     @Override
@@ -72,15 +51,8 @@ public class StockRecentCategory extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mRecentsClearAllLocation) {
-            int location = Integer.valueOf((String) newValue);
-            int index = mRecentsClearAllLocation.findIndexOfValue((String) newValue);
-            Settings.System.putIntForUser(getActivity().getContentResolver(),
-                    Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
-            mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
-        return true;
-        }
         return false;
     }
 }
+
 
